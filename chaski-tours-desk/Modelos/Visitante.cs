e1dc5ce900344c02947d8cc590dc14c45b37b0a7
Bryan <bryangod194@gmail.com>
2025-06-01 12:30:36 -0400

    plantilla para interfaz de usuario

---
+++ chaski-tours-desk/Modelos/Visitante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chaski_tours_desk.Modelos
+{
+    internal class Visitante
+    {
+        public int id { get; set; }
+        public string cod_visitante { get; set; }
+        public string tipo_visitante { get; set; }
+        public string fecha_registro { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public int Activo { get; set; }
+    }
+}
