ca7301295b883ebfb021fa2ce78bc5aa804fd867
Angel Fabricio Paredes Campos <pca2029623@est.univalle.edu>
2025-06-01 21:06:04 -0400

    alojamientos ya listos, falta reservas

---
+++ chaski-tours-desk/Modelos/Alojamiento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chaski_tours_desk.Modelos
+{
+    internal class Alojamiento
+    {
+    }
+}
---
+++ chaski-tours-desk/Modelos/Reserva.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chaski_tours_desk
+{
+    internal class Reserva
+    {
+    }
+}
