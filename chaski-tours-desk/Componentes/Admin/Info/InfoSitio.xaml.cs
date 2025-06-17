c368a68989abd6f3ce6395882afb459d5a42ebc4
Bryan <bryangod194@gmail.com>
2025-06-16 23:03:33 -0400

    Ya tenemos mapaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- chaski-tours-desk/Componentes/Admin/Info/InfoSitio.xaml.cs
+++ chaski-tours-desk/Componentes/Admin/Info/InfoSitio.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using chaski_tours_desk.Modelos;
 using System.Net.Http;
 using System.Net.Http.Json;
+using Microsoft.Maps.MapControl.WPF;
 
 
 namespace chaski_tours_desk.Componentes.Admin.Info
@@ -26,28 +27,43 @@ namespace chaski_tours_desk.Componentes.Admin.Info
     {
         private HttpClient cliente = new HttpClient();
         private string URL = "http://localhost:8000/api/sitios/";
+        private string URL_Ubi = "http://localhost:8000/api/ubicaciones/";
         public InfoSitio(int id_sitio)
         {
             InitializeComponent();
-            mostrarSitio(id_sitio);
-        }
-        private void mostrarSitio(int id_sitio) {
-            var sitio = obtenerSitio(id_sitio).Result;
-            
-            txbNombreSitio.Text = sitio.nombre;
-            txbDescConceptualSitio.Text = sitio.desc_conceptual_sitio;
-            txbDescHistoricaSitio.Text = sitio.desc_historica_sitio;
-            txbCostoSitio.Text = "Precio: \n" + (sitio.costo_sitio.ToString() == "0" ? "Gratis" : "Bs. " + sitio.costo_sitio.ToString());
-            txbTemporadaSitio.Text = sitio.temporada_recomendada;
-            txbRecomendacionSitio.Text = sitio.recomendacion_climatica;
-            txbAperturaSitio.Text = sitio.horario_apertura;
-            txbCierreSitio.Text = sitio.horario_cierre;
-            txbActivoSitio.Text = sitio.Activo == 1 ? "Activo" : "Inactivo";
+            obtenerUbicacion(obtenerSitio(id_sitio));
         }
 
-        private Task<Sitio> obtenerSitio(int id_sitio)
+        private Sitio obtenerSitio(int id_sitio)
+        {   
+            var sitio = cliente.GetFromJsonAsync<Sitio>(URL + id_sitio).Result;
+            
+            return sitio;
+        }
+
+        private void obtenerUbicacion(Sitio sitio)
         {
-            return cliente.GetFromJsonAsync<Sitio>(URL + id_sitio);
+            var ubicacion = cliente.GetFromJsonAsync<List<Ubicacion>>(URL_Ubi + sitio.id_ubicacion).Result;
+            cargarMapa(ubicacion[0]);
+        }
+
+        private  void cargarMapa(Ubicacion ubi) {
+
+            var mapControl = new Map();
+            mapControl.Center = new Location(double.Parse(ubi.latitud), double.Parse(ubi.longitud));
+            //mapControl.Center = new Location(-17.383300, -66.166700);
+            mapControl.ZoomLevel = 10;
+
+
+            var pushpin = new Pushpin()
+            {
+                Location = new Location(double.Parse(ubi.latitud), double.Parse(ubi.longitud)),
+                //Location = new Location(-17.383300, -66.166700),
+                Background = new SolidColorBrush(Colors.Red),
+            };
+
+            mapControl.Children.Add(pushpin);
+            mapa.Children.Add(mapControl);
         }
     }
 }
