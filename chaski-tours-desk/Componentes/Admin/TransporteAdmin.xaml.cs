47064598da73618e5c314bfbe6eb3d3ea5a15874
Carlos Conde <carloscondeeduardomamani@gmail.com>
2025-06-21 17:56:43 -0400

    el campo transportes ya esta validado y funciona biem, listo para revision

--- chaski-tours-desk/Componentes/Admin/TransporteAdmin.xaml.cs
+++ chaski-tours-desk/Componentes/Admin/TransporteAdmin.xaml.cs
@@ -35,17 +35,14 @@ namespace chaski_tours_desk.Componentes.Admin
 
         private void configurardatos()
         {
-            
-                txt_id.Text = trans.id_vehiculo.ToString();
-                txt_matricula.Text = trans.matricula.ToString();
-                txt_marca.Text = trans.marca.ToString();
-                txt_modelo.Text = trans.modelo.ToString();
-                txt_capacidad.Text = trans.capacidad.ToString();
-                txt_anio.Text = trans.anio.ToString();
-                txt_disponible.Text = trans.disponible.ToString();
-                txt_activo.Text = trans.activo.ToString();
-            
-            
+            txt_id.Text = trans.id_vehiculo.ToString();
+            txt_matricula.Text = trans.matricula.ToString();
+            txt_marca.Text = trans.marca.ToString();
+            txt_modelo.Text = trans.modelo.ToString();
+            txt_capacidad.Text = trans.capacidad.ToString();
+            txt_anio.Text = trans.anio.ToString();
+            txt_disponible.Text = trans.disponible.ToString();
+            txt_activo.Text = trans.activo.ToString();
         }
 
 
@@ -88,19 +85,24 @@ namespace chaski_tours_desk.Componentes.Admin
             txt_modelo.Text == "" ||
             txt_capacidad.Text == "" ||
             txt_anio.Text == "" ||
-            txt_disponible.Text == "")
+            txt_disponible.Text == ""||
+            txt_activo.Text=="")
             {
                 MessageBox.Show("llene todos los campos");
                 return false;
             }
+
             if (!int.TryParse(txt_capacidad.Text, out int capacidad) ||
                 !int.TryParse(txt_anio.Text, out int anio) ||
-                !int.TryParse(txt_disponible.Text, out int disponible))
+                !int.TryParse(txt_disponible.Text, out int disponible) ||
+                !int.TryParse(txt_activo.Text, out int activo))
             {
-                MessageBox.Show("Capacidad, Año, Disponible deben ser números válidos.");
+                MessageBox.Show("Los campos Capacidad, Año, Disponible y Activo deben ser números.");
                 return false;
             }
+
             int anioActual = DateTime.Now.Year;
+
             if (anio < 1950 || anio > anioActual + 1)
             {
                 MessageBox.Show($"El año debe estar entre 1950 y {anioActual + 1}.");
@@ -108,40 +110,62 @@ namespace chaski_tours_desk.Componentes.Admin
             }
             if (txt_matricula.Text.Length > 8)
             {
-                MessageBox.Show("La matricula debe de ser valida");
+                MessageBox.Show("La matricula debe de ser de 8 caracteres");
                 return false;
             }
-            if (txt_capacidad.Text.Length > 3)
+            if (capacidad > 80 && capacidad < 0)
             {
-                MessageBox.Show("La capacidad debe de ser valida");
+                MessageBox.Show("El campo capacidad no debe ser mayor a 80 y menor a 0");
                 return false;
             }
-            if (txt_disponible.Text != "0" && txt_disponible.Text != "1")
+            if (disponible != 0 && disponible != 1)
             {
-                MessageBox.Show("La disponibilidad debe de ser 1 o 0");
+                MessageBox.Show("El campo disponible debe de ser 1 o 0");
+                return false;
+            }
+            if (activo != 0 && activo != 1)
+            {
+                MessageBox.Show("El campo activo debe de ser 1 o 0");
                 return false;
             }
 
 
             return true;
         }
+
+        private async void mandarTransporte()
+        {
+            await ActualizarTransporteAsync();
+        }
+
         public async Task ActualizarTransporteAsync()
         {
-            Transporte transporte = new Transporte
-            {
-                id_vehiculo = int.Parse(txt_id.Text),
-                matricula = txt_matricula.Text,
-                marca = txt_marca.Text,
-                modelo = txt_modelo.Text,
-                capacidad = int.Parse(txt_capacidad.Text),
-                anio = txt_anio.Text,
-                disponible = int.Parse(txt_disponible.Text),
-                activo = int.Parse(txt_activo.Text)
-            };
-            string json = JsonSerializer.Serialize(transporte);
+            //Transporte transporte = new Transporte
+            //{
+            //    id_vehiculo = int.Parse(txt_id.Text),
+            //    matricula = txt_matricula.Text,
+            //    marca = txt_marca.Text,
+            //    modelo = txt_modelo.Text,
+            //    capacidad = int.Parse(txt_capacidad.Text),
+            //    anio = txt_anio.Text,
+            //    disponible = int.Parse(txt_disponible.Text),
+            //    activo = int.Parse(txt_activo.Text)
+            //};
+
+            trans.id_vehiculo =int.Parse(txt_id.Text);
+            trans.matricula = txt_matricula.Text;
+            trans.marca = txt_marca.Text;
+            trans.modelo = txt_modelo.Text;
+            trans.capacidad = int.Parse(txt_capacidad.Text);
+            trans.anio = txt_anio.Text;
+            trans.disponible = int.Parse(txt_disponible.Text);
+            trans.activo = int.Parse(txt_activo.Text);
+
+            string json = JsonSerializer.Serialize(trans);
+
             MessageBox.Show(json);
 
-            HttpResponseMessage response = await cliente.PutAsJsonAsync($"{URL}/{transporte.id_vehiculo}", transporte);
+            HttpResponseMessage response = await cliente.PutAsJsonAsync($"{URL}/{trans.id_vehiculo}", trans);
             if (response.IsSuccessStatusCode)
             {
                 MessageBox.Show("Transporte actualizado correctamente");
@@ -152,10 +176,6 @@ namespace chaski_tours_desk.Componentes.Admin
                 MessageBox.Show("Error al actualizar el transporte");
             }
 
-        }
-        private async void mandarTransporte()
-        {
-            await ActualizarTransporteAsync();
         }
 
         private void Cerrar_Click(object sender, RoutedEventArgs e)
